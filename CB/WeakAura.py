ffe51cf9edf3cf5250eb47cb3f2788510f6ac4b2
Paweł Jastrzębski <pawelj@iosphe.re>
2019-08-30 18:12:29 +0200

    Improved WA version check

--- CB/WeakAura.py
+++ CB/WeakAura.py
@@ -1,17 +1,18 @@
 import os
 import re
 import requests
+from lupa import LuaRuntime
 from pathlib import Path
 from . import retry
 
 
 class WeakAuraUpdater:
     def __init__(self, username):
-        self.url = re.compile('(\w+)/(\d+)')
         self.username = username
+        self.lua = LuaRuntime()
+        self.url_parser = re.compile('(\w+)/(\d+)')
         self.storage_location = []
         self.wa_outdated = []
-        self.wa_ids = []
         self.wa_list = {}
         self.locate_storage()
         self.parse_storage()
@@ -25,20 +26,18 @@ class WeakAuraUpdater:
 
     def parse_storage(self):
         for storage in self.storage_location:
-            with open(storage) as fp:
-                for _, line in enumerate(fp):
-                    if '["url"]' in line:
-                        search = self.url.search(line)
-                        if search.group(1) and search.group(2) and \
-                                (search.group(1) not in self.wa_list.keys() or
-                                 int(search.group(2)) > self.wa_list[search.group(1)]):
-                            self.wa_list[search.group(1)] = int(search.group(2))
-                            self.wa_ids.append(search.group(1))
-        self.wa_ids = list(dict.fromkeys(self.wa_ids))
+            with open(storage, 'r') as file:
+                data = file.read().replace('WeakAurasSaved = {', '{')
+            wadata = self.lua.eval(data)
+            for wa in wadata['displays']:
+                if not wadata['displays'][wa]['parent'] and wadata['displays'][wa]['url']:
+                    search = self.url_parser.search(wadata['displays'][wa]['url'])
+                    if search.group(1) and search.group(2):
+                        self.wa_list[search.group(1)] = int(search.group(2))
 
     @retry()
     def check_updates(self):
-        payload = requests.get(f'https://data.wago.io/api/check/weakauras?ids={",".join(self.wa_ids)}').json()
+        payload = requests.get(f'https://data.wago.io/api/check/weakauras?ids={",".join(self.wa_list.keys())}').json()
         for aura in payload:
             if 'username' in aura and (not self.username or aura['username'] != self.username):
                 if aura['version'] > self.wa_list[aura['slug']]:
